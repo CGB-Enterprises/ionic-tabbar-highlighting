7e0630c536e1e5e1acd0fcfbc5f445a0b548ef69
Phillip Weaver <Phillip.Weaver@CGB.com>
2020-03-25 12:40:36 -0500

    Add project to repo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController, ActionSheetController, PopoverController, ModalController, MenuController, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,60 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private alertController: AlertController,
+    private modalController: ModalController,
+    private menuController: MenuController,
+    private actionSheetController: ActionSheetController,
+    private popoverController: PopoverController,
+    private router: Router,
+    private navController: NavController
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (this.platform.is('hybrid')) {
+        this.initializeHybridSpecific();
+      }
     });
+  }
+
+  private backHandler = async () => {
+    console.log('Back button pressed!');
+    if (await this.modalController.getTop()) {
+      this.modalController.dismiss();
+    }
+    else if (await this.menuController.isOpen()) {
+      this.menuController.close();
+    }
+    else if (await this.alertController.getTop()) {
+      this.alertController.dismiss();
+    }
+    else if (await this.actionSheetController.getTop()) {
+      this.actionSheetController.dismiss();
+    }
+    else if (await this.popoverController.getTop()) {
+      this.popoverController.dismiss();
+    }
+    else if (this.router.url.startsWith('/tabs/') && this.router.url !== '/tabs/tab1') {
+      this.navController.navigateRoot('/tabs/tab1');
+    }
+    else if (this.router.url === '/tabs/tab1') {
+      navigator['app'].exitApp();
+    }
+  }
+
+  private initializeHybridSpecific() {
+    if (this.platform.is('ios')) {
+      this.statusBar.styleDefault();
+    } else {
+      this.statusBar.styleLightContent();
+      console.log('Subscribing to back button event...');
+      this.platform.backButton.subscribe(this.backHandler);
+    }
+
+    this.splashScreen.hide();
   }
 }
